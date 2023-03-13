921bdcdbc5281946d35115c3d00aba455215e0ad
ASHrayrm <116673065+ASHrayrm@users.noreply.github.com>
2023-03-13 13:18:40 +0530

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,9 +1,9 @@
-#To check whether a container is running or not
+#To check whether a container is present or not
 
 #!/bin/bash
 COUNT=$(docker ps -a | grep "fbapp" | wc -l)
 if [ $COUNT -ne 0 ]; then
-        echo "Container is running"
+        echo "Container is present"
 else
         echo "Container is not found"
 fi
